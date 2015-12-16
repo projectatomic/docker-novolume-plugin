43491d6d09ae03d9fc20093e1755789dadc623b8
Antonio Murdaca <runcom@redhat.com>
2015-12-16 10:17:37 +0100

    plugin.go: straight return dkauthz.Response
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- plugin.go
+++ plugin.go
@@ -35,7 +35,7 @@ func (p *novolume) AuthZReq(req dkauthz.Request) dkauthz.Response {
 			}
 			vf := &vfrom{}
 			if err := json.NewDecoder(bytes.NewReader(req.RequestBody)).Decode(vf); err != nil {
-				resp(err)
+				return dkauthz.Response{Err: err.Error()}
 			}
 			if len(vf.VolumesFrom) > 0 {
 				goto noallow
@@ -43,16 +43,16 @@ func (p *novolume) AuthZReq(req dkauthz.Request) dkauthz.Response {
 		}
 		res := startRegExp.FindStringSubmatch(req.RequestURI)
 		if len(res) < 1 {
-			return resp(false)
+			return dkauthz.Response{Err: "unable to find container name"}
 		}
 
 		container, err := p.client.ContainerInspect(res[1])
 		if err != nil {
-			return resp(err)
+			return dkauthz.Response{Err: err.Error()}
 		}
 		image, _, err := p.client.ImageInspectWithRaw(container.Image, false)
 		if err != nil {
-			return resp(err)
+			return dkauthz.Response{Err: err.Error()}
 		}
 		if len(image.Config.Volumes) > 0 {
 			goto noallow
@@ -66,32 +66,12 @@ func (p *novolume) AuthZReq(req dkauthz.Request) dkauthz.Response {
 			goto noallow
 		}
 	}
-	return resp(newResponse(true, "", ""))
+	return dkauthz.Response{Allow: true}
 
 noallow:
-	return resp(newResponse(false, "volumes are not allowed", ""))
+	return dkauthz.Response{Msg: "volumes are not allowed"}
 }
 
 func (p *novolume) AuthZRes(req dkauthz.Request) dkauthz.Response {
-	return resp(newResponse(true, "", ""))
-}
-
-// TODO(runcom): awful...
-func newResponse(allow bool, msg string, err string) dkauthz.Response {
-	res := dkauthz.Response{}
-	res.Allow = allow
-	res.Msg = msg
-	res.Err = err
-	return res
-}
-
-func resp(r interface{}) dkauthz.Response {
-	switch t := r.(type) {
-	case error:
-		return dkauthz.Response{Err: t.Error()}
-	case dkauthz.Response:
-		return t
-	default:
-		return dkauthz.Response{Err: "bad value writing response"}
-	}
+	return dkauthz.Response{Allow: true}
 }
